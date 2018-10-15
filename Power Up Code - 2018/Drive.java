5725efba85cb42089280e73bf339c0acc647f953
Zak <zakgriffin1209@gmail.com>
2018-10-15 09:06:11 -0400

    dead zones and stuff

--- Power Up Code - 2018/Drive.java
+++ Power Up Code - 2018/Drive.java
@@ -9,6 +9,8 @@ public class Drive {
 	
 	private final static double DRIVE_DOWN_SCALE = 0.6;
 	private final static double CURVE_FACTOR = 0.7; // -0.5 to 1 range
+	private final static double DEAD_ZONE = 0.2; // 0 to 1 range
+	private final static int DEAD_ZONE_BUTTON = 2;
 	
 	public Drive(Spark leftDrive, Spark rightDrive, Joystick leftJoy, Joystick rightJoy, Joystick twistJoy) {
 		 drive = new DifferentialDrive(leftDrive, rightDrive);
@@ -25,8 +27,9 @@ public class Drive {
 	
 	public void teleopPeriodic() {
 		double twist = twistJoy.getTwist();
-		if(Math.abs(twist) > 0.25) {
+		if(Math.abs(twist) > DEAD_ZONE) {
 			// allows elevator operator to override rotation
+			twist = deadZone(twist, DEAD_ZONE);
 			drive.tankDrive(twist, -twist);
 		} else {
 			// drive functionality
@@ -35,6 +38,11 @@ public class Drive {
 			double speedR;
 			double speedL;
 			
+			if(rightJoy.getRawButton(DEAD_ZONE_BUTTON)) {
+				// optimize control for dead zones
+				throtR = deadZone(throtR, DEAD_ZONE);
+				throtL = deadZone(throtL, DEAD_ZONE);
+			}
 			if(leftJoy.getTrigger()) {
 				// drive linear
 				speedR = throtR;
@@ -65,5 +73,14 @@ public class Drive {
 	
 	public void autonomousPeriodic() {
 		
+	}
+	private double deadZone(double joyInput, double deadZone) {
+		if(joyInput > 0) {
+			return (joyInput - 1) / (1 - deadZone) + 1;
+		}
+		else if(joyInput < 0) {
+			return (joyInput + 1) / (1 - deadZone) - 1;
+		}
+		else return 0;
 	}
 }
